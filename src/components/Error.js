29eefb8fe582c4b817dd08165b5d80461309f54d
Aleksandr <duxard@gmail.com>
2019-11-15 01:10:02 -0500

    step 2

---
+++ src/components/Error.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import img from '../error.png';
+
+const ErrorMsg = () => {
+  return (
+    <>
+      <img src={img} alt='error'></img>
+    </>
+  );
+}
+
+export default ErrorMsg;
--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,12 +1,73 @@
 import React, { Component } from 'react';
-import Post from './Post';
+import InstaService from '../services/InstaService';
+import User from './User';
+import ErrorMsg from './Error';
 
 export default class Posts extends Component {
+  instaServiceInstance = new InstaService();
+  state = {
+    posts: [],
+    error: false
+  };
+
+  componentDidMount() {
+    this.updatePosts();
+
+  }
+
+  updatePosts() {
+    this.instaServiceInstance.getAllPosts()
+      .then(this.onPostsLoaded)
+      .catch(this.onError);
+  }
+
+  onPostsLoaded = (posts) => {
+    console.log(posts);
+    this.setState({
+      posts,
+      error: false
+    });
+  }
+
+  onError = (err) => {
+    this.setState({
+      error: true
+    });
+  }
+
+  renderItems(array) {
+    return array.map(item => {
+      const { name, altname, photo, src, alt, descr, id } = item;
+      return (
+        <div key={id} className="post">
+          <User src={photo}
+                alt={altname}
+                name={name}
+                min
+          />
+          <img src={src} alt={alt} />
+          <div className="post__name">
+            {name}
+          </div>
+          <div className="post__descr">
+            <p>{descr}</p>
+          </div>
+        </div>
+      );
+    });
+  }
+
   render() {
+    const {error, posts} = this.state;
+    const items = this.renderItems(posts);
+
+    if(error) {
+      return <ErrorMsg />
+    }
+
     return (
       <div className="left">
-        <Post src="https://is4-ssl.mzstatic.com/image/thumb/Purple71/v4/a2/40/1a/a2401ae5-f5a7-29ed-41b8-cda9d6e36623/source/256x256bb.jpg" alt="one"/>
-        <Post src="https://vignette.wikia.nocookie.net/gardenpaws/images/4/46/Purple_Flower.png/revision/latest?cb=20180915140453" alt="second"/>
+        {items}
       </div>
     );
   }
